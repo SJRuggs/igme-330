bd244174c09ee554421f2d478e108b0e817f5319
SJRuggs <sjruggs03@gmail.com>
2023-09-17 23:02:10 -0400

    hw4 done

--- ruggiero-s-hw1/src/main.js
+++ ruggiero-s-hw1/src/main.js
@@ -1,45 +1,53 @@
 import * as utils from "./utils.js";
 
-const init = () => {
-
-    // generation function
-    const generateTechno = count => {
+const init = () =>
+{
+    const generateTechno = (json, count) =>
+    {
         let babble = "";
         for (let i = 0; i < count; i++)
         {
             babble += (i > 0) ? "<br>" : "";
             babble += `
-            ${utils.randomElement(json["words-one"])}
-            ${words[1][Math.floor(Math.random() * words[1].length)]}
-            ${words[2][Math.floor(Math.random() * words[2].length)]}
+            ${utils.randomElement(json.words[0])}
+            ${utils.randomElement(json.words[1])}
+            ${utils.randomElement(json.words[2])}
             `;
         }
         return babble;
     };
 
-    let json = new XMLHttpRequest();
-    json.onload = (e) =>
+    const loadBabble = (url, callback) =>
     {
-        console.log(`In onload - HTTP Status Code = ${e.target.status}`);
-        json = e.target.responseXML;
+        let json;
+        const xhr = new XMLHttpRequest();
+        xhr.onload = (e) =>
+        {
+            console.log(`In onload - HTTP Status Code = ${e.target.status}`);
+            const text = e.target.response;
+            try { json = JSON.parse(text); }
+            catch (e) { console.log(e); return; }
+            callback(json);
+        }
+        xhr.onerror = (e) => console.log(`In onerror - HTTP Status Code = ${e.target.status}`);
+        xhr.open("GET", url);
+        xhr.send();
+    };
 
-        console.log(json);
-
-        // find references
+    const babbleLoaded = (json) =>
+    {
         const giveOne = document.querySelector("#give-one");
         const giveFive = document.querySelector("#give-five");
         const output = document.querySelector("#output");
-
-        // assign onclick handlers
-        giveOne.onclick = () => output.innerHTML = generateTechno(1);
-        giveFive.onclick = () => output.innerHTML = generateTechno(5);
-
-        // initial generation
-        output.innerHTML = generateTechno(1);
+        giveOne.onclick = () => output.innerHTML = generateTechno(json, 1);
+        giveFive.onclick = () => output.innerHTML = generateTechno(json, 5);
+        output.innerHTML = generateTechno(json, 1);
     }
-    json.onerror = (e) => console.log(`In onerror - HTTP Status Code = ${e.target.status}`);
-    json.open("GET", "./data/babble-data.json");
-    json.send();   
+
+    loadBabble("./data/babble-data.json", babbleLoaded);
+    
 }
+
+
 
 init();
