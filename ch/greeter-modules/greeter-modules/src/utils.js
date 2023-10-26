fa2e70d9e5714fa0fb1e98e8575b9ccd916a35d6
SJRuggs <sjruggs03@gmail.com>
2023-10-26 18:08:26 -0400

    node start

---
+++ ch/greeter-modules/greeter-modules/src/utils.js
@@ -0,0 +1,12 @@
+const defaultName = "Mr. X";
+
+const doubleIt = val =>  val * 2;
+
+const formatGreeting = (greeting, name, forcefully) => {
+  const recipient  = name ? name : defaultName;
+  const str = `${greeting} ${recipient}`;
+  return forcefully ? `${str.toUpperCase()}!` : str;
+};
+
+// export our "public" symbols, everything else in this file is "private" by default
+export { defaultName, doubleIt, formatGreeting };
